ef0beb24f792a41e1c5df6ec508f7b28c9888420
clara-martinez <123569785+clara-martinez@users.noreply.github.com>
2023-02-06 12:03:12 -0500

    vars

---
+++ app.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded"), () => {
+  const cardsAdj = [
+    {name '1',
+    img: 'images/1.jpg'
+  },
+    {name '1',
+    img: 'images/1.jpg'
+  },
+    {name '1',
+    img: 'images/1.jpg'
+  },
+    {name '1',
+    img: 'images/1.jpg'
+  },
+    {name '1',
+    img: 'images/1.jpg'
+  },
+    {name '1',
+    img: 'images/1.jpg'
+   },
+]
+
+});
