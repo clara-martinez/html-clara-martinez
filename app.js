b30f573fc410c46743dcbe8316e5c6b6913214aa
clara-martinez <123569785+clara-martinez@users.noreply.github.com>
2023-02-08 09:52:31 -0500

    vars

--- app.js
+++ app.js
@@ -3,21 +3,26 @@ document.addEventListener("DOMContentLoaded"), () => {
     {name '1',
     img: 'images/1.jpg'
   },
-    {name '1',
+    {name '2',
     img: 'images/1.jpg'
   },
-    {name '1',
+    {name '3',
     img: 'images/1.jpg'
   },
-    {name '1',
+    {name '4',
     img: 'images/1.jpg'
   },
-    {name '1',
+    {name '5',
     img: 'images/1.jpg'
   },
-    {name '1',
+    {name '6',
     img: 'images/1.jpg'
    },
+   {name '7',
+    img: 'images/1.jpg'
+   },{name '8',
+   img: 'images/1.jpg'
+  },
 ]
 
 });
